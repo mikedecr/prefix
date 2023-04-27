00f907866ba305479d80aff83888ed01d25aafff
mikedecr <mgdecrescenzo@gmail.com>
2023-04-26 21:14:41 -0500

    more arithmetic operators

--- R/arithmetic.R
+++ R/arithmetic.R
@@ -1,6 +1,29 @@
 
 
+#' prefix alias for `a + b`
+#' @export
+add = `+`
+
+#' prefix alias for `a - b`
+#' @export
+minus = `-`
+
 #' prefix alias for `a * b`
 #' @export
 mult = `*`
 
+#' prefix alias for `a / b`
+#' @export
+div = `/`
+
+#' prefix alias for `a^b`
+#' @export
+pow = `^`
+
+#' prefix alias for `a %% b`
+#' @export
+mod = `%%`
+
+#' prefix alias for `a %/% b`
+#' @export
+floordiv = `%/%`
